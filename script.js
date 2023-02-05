11fb2a3f19cc54aca932b9aa0e603769636f9d42
it4station@gmail.com <it4station@gmail.com>
2023-02-05 15:45:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+function onDragStart(event) {
+  event.dataTransfer.setData("text", event.target.id);
+  //récupérer l'id de la classe parente
+  let temp = document.getElementById(event.target.id).parentElement;
+
+  //en fonction de classe
+  if (temp.className == "ListFrog M") {
+    let el1 = document.querySelector("#dropzone_M");
+    if (!el1.hasAttribute("onDragOver")) {
+      el1.setAttribute("onDragOver", "onDragOver(event)");
+    }
+    let el2 = document.querySelector("#dropzone_F");
+    el2.removeAttribute("onDragOver");
+    let el3 = document.querySelector("#ListFrog_F");
+    el3.removeAttribute("onDragOver");
+    let el4 = document.querySelector("#ListFrog_M");
+    if (!el4.hasAttribute("onDragOver")) {
+      el4.setAttribute("onDragOver", "onDragOver(event)");
+    }
+  }
+  if (temp.className == "ListFrog F") {
+    let el1 = document.querySelector("#dropzone_F");
+    if (!el1.hasAttribute("onDragOver")) {
+      el1.setAttribute("onDragOver", "onDragOver(event)");
+    }
+    let el2 = document.querySelector("#dropzone_M");
+    el2.removeAttribute("onDragOver");
+    let el3 = document.querySelector("#ListFrog_M");
+    el3.removeAttribute("onDragOver");
+    let el4 = document.querySelector("#ListFrog_F");
+    if (!el4.hasAttribute("onDragOver")) {
+      el4.setAttribute("onDragOver", "onDragOver(event)");
+    }
+    //-->désactiver le drag-over/drag-drop
+  }
+    event.currentTarget.style.backgroundColor = "yellow";
+  
+}
+function onDragEnd(event) {
+  event.currentTarget.style.backgroundColor = "#4AAE9B";
+}
+
+function onDragOver(event) {
+  //console.log(event.currentTarget.className);
+  event.preventDefault();
+}
+
+function onDragEnter(event) {}
+
+function onDrop(event) {
+  const id = event.dataTransfer.getData("text");
+
+  const draggableElement = document.getElementById(id);
+  //récupérer l'enfant si dropzone globale
+  const dropzone = event.target;
+  if (dropzone.className == "PlaceHolder" || dropzone.className.includes("ListFrog")) {
+    dropzone.appendChild(draggableElement);
+    event.dataTransfer.clearData();
+  }
+}
+
+//function get tokenId list from user wallet
+//get json metadata
